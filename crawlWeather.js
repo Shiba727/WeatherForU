00c66b3bb84da8e2cd15ab93883cefe8b658cc31
Janice <a6513512@gmail.com>
2018-07-22 15:14:35 +0800

    issue for crawling weather data

--- crawlWeather.js
+++ crawlWeather.js
@@ -1,8 +1,44 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-const url = 'https://www.cwb.gov.tw/V7/forecast/';
+request({
+    url: "https://www.cwb.gov.tw/V7/forecast/",
+    method: "GET"
+}, function(err, response, body) { /* Callback function */
+    if (err || !body) {
+        return;
+    }
+    //  to load the HTML. 
+    $ = cheerio.load(body);
+    var citiesWeather = [];
+    var cities = $('div.NorthArea table.N_AreaList tbody tr#KeelungList td a');
 
-request(url, (err, res, body) => {
-  console.log(body)
-})
+    console.log($(cities).text());
+    // console.log(citiesWeather);
+});
+
+
+
+
+
+
+
+
+
+
+// const request = require('request');
+// const url = 'http://opendata.cwb.gov.tw/opendataapi?dataid=F-C0032-001&authorizationkey=CWB-C7CCB1D0-AD5B-4DFD-A02D-7E7CBEC72DB7';
+
+// var xml2js = require('xml2js');
+// var parseString = xml2js.parseString;
+// var xml;
+
+// request(url,function(err,res,body){
+// 	// console.log(body);
+// 	xml = body;
+// })
+
+// parseString(xml, function (err, result) {
+//     console.log(result);
+// });
+
